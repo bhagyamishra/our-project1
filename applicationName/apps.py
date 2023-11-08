8eb75b0a9791818cbe092e66537761491fb11a5b
bhagyamishra <bhagyamishra25@gmail.com>
2023-11-08 23:48:07 +0530

    first commit

---
+++ applicationName/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ApplicationnameConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'applicationName'
---
+++ applicationName/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse 
+def jampandu (request):
+    return HttpResponse(' <h1><marquee>hi jampandu how are you </h1></marquee>') 
+
